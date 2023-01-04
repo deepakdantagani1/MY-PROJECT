70ab0ca663eb262420507457f45db6c5c93678d8
deepakdantagani <deepakn1717@gmail.com>
2023-01-04 08:58:08 -0600

    object encryption added

--- 3-AWS/S3/11-objectEncryption.js
+++ 3-AWS/S3/11-objectEncryption.js
@@ -5,9 +5,18 @@
  *          ==> object is encrypted server side
  *          ==> encryption type is ASE-256
  *          ==> must set header "x-amz-server-side-encryption":"AES256"
+ *
  * sse-kms  ==> encryotion keys managed by AWS KMS
  *          ==> adventages are user controla nd alos audit keys using cloutTrail
  *          ==> must set header to "x-amz-server-side-encryption":"aws:kms"
  *          ==> limitions are needs another additional requests to KMS services.
  *          ==> 5500, 10000, 30000 req/s based on the region
+ *
+ * sse-c    ==> fully managed by the customer outside AWS,
+ *          ==> https must be used and the encryption key must be passed through https headers
+ *
+ * ---client side encryotion---
+ * ---encryption in transit---
+ * ssl/tls, https endpoint => encryption on flight
+ *
  */
