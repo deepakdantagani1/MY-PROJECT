5686d482daf537b158de24dda5c2a6d51648952a
deepakdantagani <deepakn1717@gmail.com>
2023-01-06 18:27:58 -0600

    more changes

---
+++ 3-AWS/ebs/4-ebsUsingCloudFormation.js
@@ -0,0 +1,4 @@
+/**
+ * cloudFormation: this is used to provision other AWS services
+ * we can define cloudFormation resources in .ebextensions to provision elasticCache, an s3 bucket, anyting we want
+ */
