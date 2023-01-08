84c01159078fd67f317efdeeda608157d89896b8
deepakdantagani <deepakn1717@gmail.com>
2023-01-07 19:54:10 -0600

    more changes

---
+++ 3-AWS/cicd/1-overview.js
@@ -0,0 +1,24 @@
+/**
+ * AWS CodeCommit - store our code
+ * AWS code Pipeline - automating our pipeline from code to elastic deanstalk
+ * AWS codeBuild - building and testing our code
+ * AWS code deploy - deploying the code to EC2 instances (not elastic beanstack)
+ * AWS codeStar - nanage software develpoment activities in one place
+ * AWS codeArtifact - store, publish, and share software packages
+ * AWS codeGuru - automated code reviews using Machine Learning
+ *
+ * ---continuous intigration (CI)---
+ * push the code to repo
+ * testing/building servers check the as soon as it's pushed
+ * developers gets feedback
+ * find and fix bugs
+ * deliver faster as the code is tested
+ * deploy often
+ *
+ * ---continous deployment (CD)---
+ * ensures that the software can be released reliably whenever needed
+ * deployment happens often and are quick
+ * automated deployment
+ *
+ * https://share.icloud.com/photos/001N_CgScN1TpLtNsa-flXeQQ
+ */
