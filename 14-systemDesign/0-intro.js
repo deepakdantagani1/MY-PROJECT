4506043bcb33e59dcd037f4aea1dffd7c85e9eb2
deepakdantagani <deepakn1717@gmail.com>
2023-02-12 12:07:11 -0600

    more changes

---
+++ 14-systemDesign/0-intro.js
@@ -0,0 +1,11 @@
+/**
+ * ==> scalability
+ * ==> horizontal and verical scaling ==> servers, seprate database, virtual machine, load balancer
+ * stateless
+ * ==> failover strategies ==> cold storage, warm standby, hot standby
+ * ==> mongoDb ==> document, collection, shardds, replica sets, mongo config, query router
+ * define capped, shards, validations, collection(sorting), Storage Engine, Compression
+ *
+ *
+ *
+ */
